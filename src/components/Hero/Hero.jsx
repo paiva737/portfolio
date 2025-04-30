2e6c6d5f5cd6359a23c636f32f2261e18972722c
paiva737 <rafaelpaiva636@gmail.com>
2025-04-29 22:03:07 -0300

    Adicionando aba hero

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+      <h1>Olá, eu sou Rafael</h1>
+    </section>
+  );
+};
+
+export default Hero;
