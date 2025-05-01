4e256576fa8cad5da13f1ad7f071dc7ba4e52f86
paiva737 <rafaelpaiva636@gmail.com>
2025-04-30 21:34:53 -0300

    estilizando skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,19 +10,21 @@ import {
   SiBootstrap,
   SiMongodb,
 } from 'react-icons/si';
+import { FaTools } from 'react-icons/fa'; 
 import 'aos/dist/aos.css';
 
 const Skills = () => {
   return (
     <section className={styles.skills} id="skills">
       <h2 data-aos="fade-up" data-aos-offset="300" data-aos-duration="800">
+      <FaTools style={{ marginRight: '8px' }} />
         Habilidades
       </h2>
       <div className={styles.underline} data-aos="fade-up" data-aos-offset="300" data-aos-duration="800"></div>
 
       <div className={styles.circle} data-aos="zoom-in" data-aos-offset="300" data-aos-duration="1000">
         <div className={styles.rotate}>
-          <div className={styles.item}><SiHtml5 color="#E44D26" title="HTML5" /></div>
+          <div className={styles.item}><SiHtml5 color="#E44D26" title="HTML5"/></div>
           <div className={styles.item}><SiCss3 color="#1572b6" title="CSS3" /></div>
           <div className={styles.item}><SiJavascript color="#F7DF1E" title="JavaScript" /></div>
           <div className={styles.item}><SiReact color="#61DAFB" title="React" /></div>
